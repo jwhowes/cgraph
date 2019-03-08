513b50c4987457935bdd5ab3fe961756a94726b8
benapier <napier.ben.e@gmail.com>
2019-03-08 16:33:14 +0000

    Added DirectedGraph (empty)

---
+++ DirectedGraph.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ben Napier on 08/03/2019.
+//
+
+#include "DirectedGraph.h"
---
+++ DirectedGraph.h
@@ -0,0 +1,14 @@
+//
+// Created by Ben Napier on 08/03/2019.
+//
+
+#ifndef CGRAPH_DIRECTEDGRAPH_H
+#define CGRAPH_DIRECTEDGRAPH_H
+
+
+class DirectedGraph {
+
+};
+
+
+#endif //CGRAPH_DIRECTEDGRAPH_H
