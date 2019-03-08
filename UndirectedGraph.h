610c3dae5ba5f2ef8ddf6ffd48416499aee4d611
benapier <napier.ben.e@gmail.com>
2019-03-08 18:24:19 +0000

    Skeleton of UndirectedGraph class

--- UndirectedGraph.h
+++ UndirectedGraph.h
@@ -6,8 +6,23 @@
 #define CGRAPH_UNDIRECTEDGRAPH_H
 
 
-class UndirectedGraph {
+#include <tuple>
+#include <vector>
+#include "Node.h"
 
+
+class UndirectedGraph {
+private:
+    std::vector<int> node_vector;
+    std::vector<std::tuple<int, int>> adjancency_vector;
+public:
+    void AddNode(int index);
+    void AddEdge(int from_index, int to_index);
+    std::vector<int> GetNeighbors(int index);
+    std::vector<int> GetEdges();
+    std::vector<int> GetNodes();
+    int GetNodeCount();
+    int GetEdgeCount();
 };
 
 
