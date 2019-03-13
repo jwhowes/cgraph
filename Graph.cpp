8e3b199aa7593d526476cf973b366a3ee5ef5b0f
benapier <napier.ben.e@gmail.com>
2019-03-13 16:07:40 +0000

    Renamed class and added functions * Renamed Undir.. to Graph * Added AddNode and AddEdge

---
+++ Graph.cpp
@@ -0,0 +1,53 @@
+//
+// Created by Ben Napier on 13/03/2019.
+//
+
+#include "Graph.h"
+
+void Graph::AddEdge(int from, int to, int weight) {
+    if (from == to) return;
+    bool exists = false;
+    for (auto &node : node_vector) {
+        if (node.get_data() == from || node.get_data() == to) {
+            if (exists) {
+                for (auto &edge_vector : edge_vector_vector) {
+                    if (std::get<0>(edge_vector) == from) {
+                        for (auto &edge : std::get<1>(edge_vector)) {
+                            if (edge.to == to) {
+                                edge.weight = weight;
+                                return;
+                            }
+                        }
+                        std::get<1>(edge_vector).push_back(Edge(to, weight));
+                        return;
+                    }
+                }
+            } else {
+                exists = true;
+            }
+        }
+    }
+    std::cerr << "cgraph: One of the nodes entered does not exist.";
+}
+
+void Graph::AddNode(int data) {
+    if (data < 0) return;
+    for (auto &node : node_vector) {
+        if (node.get_data() == data) {
+            node = Node(data);
+            return;
+        }
+    }
+    node_vector.emplace_back(Node(data));
+    edge_vector_vector.emplace_back();
+    std::get<0>(edge_vector_vector[edge_vector_vector.size() - 1]) = data;
+}
+
+std::vector<Edge> Graph::Neighbours(int data) {
+    for (auto edge_vector : edge_vector_vector) {
+        if (std::get<0>(edge_vector) == data && !std::get<1>(edge_vector).empty()) {
+            return std::get<1>(edge_vector);
+        }
+    }
+    return std::vector<Edge>();
+}
---
+++ Graph.h
@@ -0,0 +1,61 @@
+//
+// Created by Ben Napier on 13/03/2019.
+//
+
+#ifndef CGRAPH_GRAPH_H
+#define CGRAPH_GRAPH_H
+#include <vector>
+#include <tuple>
+#include <iostream>
+#include "Node.h"
+
+struct Edge {
+    int to; // Node to
+    int weight;
+
+    Edge(int p_to, int p_weight) {
+        to = p_to;
+        weight = p_weight;
+    }
+};
+
+class Graph {
+private:
+    std::vector<Node> node_vector;
+    std::vector<std::tuple<int, std::vector<Edge>>> edge_vector_vector;
+public:
+    /**
+     * Adds an edge between two nodes.
+     * Will overwrite existing edges.
+     * @param from the starting node
+     * @param to the ending node
+     * @param weight the weight of the edge
+     */
+    void AddEdge(int from, int to, int weight);
+
+    /**
+     * Adds a node.
+     * Will overwrite a nodes labels (not edges) if a node with the same data already exists.
+     * @param data the data of the node (>= 0)
+     */
+    void AddNode(int data);
+
+    /**
+     * Returns all nodes with a direct edge from the node.
+     * @param data the node index.
+     * @return a vector of nodes adjacent.
+     */
+    std::vector<Edge> Neighbours(int data);
+
+    void Debug() {
+        for (auto i : edge_vector_vector) {
+            std::cout << std::get<0>(i) << ": ";
+            for (auto j : std::get<1>(i)) {
+                std::cout << j.to << " (" << j.weight << ")" << ",";
+            }
+            std::cout << std::endl;
+        }
+    }
+};
+
+#endif //CGRAPH_GRAPH_H
